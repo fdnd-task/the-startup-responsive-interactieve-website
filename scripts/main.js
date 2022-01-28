0bf66840a7e12b1cf55bbc4f0564e53a09dcfbeb
M4TThys123 <70703948+M4TThys123@users.noreply.github.com>
2022-01-28 13:48:27 +0100

    merge jwz

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
 // STATIC SWIPER
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1.5,
-    spaceBetween: 30,
-    slidesPerGroup: 1,
-    loop: true,
-    centeredSlides: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // loop: true,
+    // centeredSlides: true,
     loopFillGroupWithBlank: true,
+    
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -14,4 +14,18 @@ var swiper = new Swiper(".mySwiper", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+        // 1024: {
+        //   slidesPerView: 5,
+        //   spaceBetween: 50,
+        // },
+      },
   });
