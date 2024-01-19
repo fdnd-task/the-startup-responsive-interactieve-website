9c1b8d0e1b8d0103f1d950680c358b8505681c4b
SamaraFellaDina <sammybensink@gmail.com>
2024-01-19 09:30:09 +0100

    formulier verbeterd

--- script/script.js
+++ script/script.js
@@ -2,6 +2,7 @@ var buttonFinishForm = document.querySelector(".form-finish");
 var DownloadFormInput = document.querySelector(".download-form-input")
 var DownloadFormComplete = document.querySelector(".download-form .download-form-complete")
 
+
 buttonFinishForm.addEventListener("click", function(e) {
     console.log("clicked!!");
     DownloadFormComplete.classList.add("download-form-complete-in");
@@ -13,3 +14,15 @@ DownloadFormComplete.addEventListener("animationend", function() {
     console.log("Animation complete!");
     DownloadFormComplete.classList.add("download-form-complete-show");
 })
+
+var emailInput = document.getElementById("ename");
+var form = document.querySelector('form');
+
+
+emailInput.addEventListener("input", function(e){
+    if (FormData.checkValidity()) {
+        console.log()
+    }
+
+})
+
